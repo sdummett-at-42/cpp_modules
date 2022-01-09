b8339179f016e1e0948d0fffaa52baa4d3dcf3fe
Stone <stone.dummett@gmail.com>
2022-01-09 21:07:23 +0100

    Add 00/ex00/megaphone.cpp

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <cstring>
 
-int	main(void)
+
+int	main(int ac, char **av)
 {
-	std::cout << "Hello world!" << std::endl;
+	int	i;
+	int	j;
+	int	len;
+
+	if (ac > 1)
+	{
+		i = 1;
+		while (i < ac)
+		{
+			j = 0;
+			len = std::strlen(av[i]);
+			while (j < len)
+			{
+				std::cout << (char)toupper(av[i][j]);
+				j++;
+			}
+			std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE NOISE *" << std::endl;
 	return (0);
 }
