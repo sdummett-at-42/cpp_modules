195c63a0e7f7772d23e6692ef4636411f2358270
Stone <stone.dummett@gmail.com>
2022-01-11 18:31:52 +0100

    Working on module_01/ex04

---
+++ module_01/ex04/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 17:35:01 by sdummett          #+#    #+#             */
+/*   Updated: 2022/01/11 18:31:21 by sdummett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+int	main(int ac, char **av) {
+
+	std::string s1 = "First";
+	std::string s2 = "Second";
+	// OPEN FILE 
+	// PUT IT TO A STRING
+	// REPLACE WHAT NEEDS TO BE REPLACED
+	// OPEN 2ND FILE
+	// AND PUT THE STRING IN IT
+	return (0);
+}
