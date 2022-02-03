e7ab8df676c016458b5c784623342593ea7fce6b
Stone <stone.dummett@gmail.com>
2022-02-03 14:13:59 +0100

    Done module06/ex00

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -6,101 +6,90 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 17:34:48 by sdummett          #+#    #+#             */
-/*   Updated: 2022/02/02 21:02:02 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/02/03 14:12:46 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
 #include <iostream>
 #include <cstdlib>
 
-#define	ERROR	-1
-#define	CHAR	1
-#define INT		2
-#define FLOAT	3
-#define DOUBLE	4
-
-// To test :
-// -inff, +inff, nanf
-// -inf, +infm, nan
-
-void	convertChar(char *str) {
-	char c = str[0];
-
-	std::cout << "char   : " << static_cast<char>(c) << std::endl;
-	std::cout << "int    : " << static_cast<int>(c) << std::endl;
-	std::cout << "float  : " << static_cast<float>(c) << std::endl;
-	std::cout << "double : " << static_cast<double>(c) << std::endl;
-}
-void	convertInt(char *str) {
-	int i = atoi(str);
-
-	std::cout << "char   : " << static_cast<char>(i) << std::endl;
-	std::cout << "int    : " << static_cast<int>(i) << std::endl;
-	std::cout << "float  : " << static_cast<float>(i) << std::endl;
-	std::cout << "double : " << static_cast<double>(i) << std::endl;
-}
-void	convertFloat(char *str) {
-	float f = atof(str);
-
-	std::cout << "char   : " << static_cast<char>(f) << std::endl;
-	std::cout << "int    : " << static_cast<int>(f) << std::endl;
-	std::cout << "float  : " << static_cast<float>(f) << std::endl;
-	std::cout << "double : " << static_cast<double>(f) << std::endl;
-}
-void	convertDouble(char *str) {
-	double d = atof(str);
-
-	std::cout << "char   : " << static_cast<char>(d) << std::endl;
-	std::cout << "int    : " << static_cast<int>(d) << std::endl;
-	std::cout << "float  : " << static_cast<float>(d) << std::endl;
-	std::cout << "double : " << static_cast<double>(d) << std::endl;
-}
-
-int		getType(char *str) {
-	if (strlen (str) == 1 && (str[0] >= '0' && str[0] <= '9'))
-		return (INT);
-
+bool	is_num(char *argv) {
 	int i = 0;
-	
-	while (str[i] != '\0') {
 
-		// std::cout << str[i];
+	while (argv[i])
+		i++;
+	if (argv[--i] == 'f')
+		argv[i] = '\0';
+	i = 0;
+	while (argv[i])
+	{
+		if ((argv[i] > '9' || argv[i] < '0') && argv[i] != '.')
+			return false;
 		i++;
 	}
-	return -1;
+	return true;
 }
 
-void	displayLiteral(char *str) {
-	int	type = getType(str);
+void	setValues(char & c, int & i, float & f, double & d, char *str) {
 	
-	if (type == CHAR) {
-		convertChar(str);
+	if (str[0] && !str[1]) {
+		c = static_cast<char>(str[0]);
+		i = static_cast<int>(str[0]);
+		f = static_cast<float>(str[0]);
+		d = static_cast<double>(str[0]);
 	}
-	else if (type == INT) {
-		convertInt(str);
-	}
-	else if (type == FLOAT) {
-		convertFloat(str);
-	}
-	else if (type == DOUBLE) {
-		convertDouble(str);
-	}
-	else if (type == ERROR) {
-		std::cout << "[ Type conversion impossible ]" << std::endl;
-	}
+	else {
+		c = static_cast<char>(std::atoi(str));
+		i = static_cast<int>(std::atoi(str));
+		f = static_cast<float>(std::atof(str));
+		d = static_cast<double>(std::atof(str));
+	} 
 }
 
+void	displayValues(char *str) {
+
+	char	c;
+	int		i;
+	float	f;
+	double	d;
+
+	setValues(c, i, f, d, str);
+
+	std::cout << "char   : ";
+	if ((c == 0) && (str[0] != '0'))
+		std::cout << "impossible" << std::endl;
+	else if (!(std::isprint(c)))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << "'" << c << "'" << std::endl;
+
+	std::cout << "int    : ";
+	if ((c == 0) && (str[0] != '0'))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << i << std::endl;
+
+	std::cout << "float  : ";
+	if ((f == 0) && (str[0] != '0'))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << f << "f" << std::endl;
+
+	std::cout << "double : ";
+	if ((d == 0) && (str[0] != '0'))
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << d << std::endl;
+		
+}
 
 int main(int ac, char **av) {
-	(void)ac;
-	(void)av;
 
 	if (ac != 2) {
 			std::cout << "Usage: ./convert literal" << std::endl;
 			std::cout << "Literal can be a char, a int, a float or a double" << std::endl;
 			return (0);
 	}
-	displayLiteral(av[1]);
+	displayValues(av[1]);
 	return 0;
 }
