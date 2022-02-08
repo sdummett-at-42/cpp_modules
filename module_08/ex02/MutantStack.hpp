30580f42556cad6bf8cd310f1749a7e142e49461
Stone <stone.dummett@gmail.com>
2022-02-08 17:37:17 +0100

    Done module08/ex02

---
+++ module_08/ex02/MutantStack.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 16:25:13 by sdummett          #+#    #+#             */
+/*   Updated: 2022/02/08 17:10:06 by sdummett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class	MutantStack : public std::stack<T>
+{
+	public:
+		typedef	typename std::deque<T>::iterator iterator;
+		iterator	begin();
+		iterator	end();
+};
+
+template <typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin()
+{
+	return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::end()
+{
+	return (this->c.end());
+}
+
+#endif
